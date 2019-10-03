93da8e5b950cebf5ef21b5e55ce444870811c0b7
svenskapojkarna <hytonenaleksi@gmail.com>
2019-10-03 14:49:02 +0300

    ...

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -8,7 +8,8 @@ const app = (props) => {
         {name: 'Aleksi', age: 23},
         {name: 'Arttu', age: 23},
         {name: 'Matias', age: 23}
-      ]
+      ],
+    showPersons: false
     }
   );
   const nameChangeHandler = (newName) => {
@@ -31,14 +32,26 @@ const app = (props) => {
     })
   }
 
+  const togglePersonsHandler = () => {
+    setPersonsState({
+      persons: personsState.persons,
+      showPersons: !personsState.showPersons
+    })
+    console.log(personsState.showPersons)
+  }
+
   return (
     <div className="App">
       <h1>Hello, I'm react app.</h1>
       <p>This is really working.</p>
-      <button className="Switchbutton" onClick={nameChangeHandler.bind(this, 'Albertintti')}>Switch name</button>
-      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} changed={switchNameHandler}/>
-      <Person name={personsState.persons[1].name} age={personsState.persons[1].age} click={nameChangeHandler.bind(this, 'Albertti')}>My hobbies: Trains</Person>
-      <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
+      <button className="Switchbutton" onClick={togglePersonsHandler}>Switch name</button>
+      {personsState.showPersons ?
+      <div>
+        <Person name={personsState.persons[0].name} age={personsState.persons[0].age} changed={switchNameHandler}/>
+        <Person name={personsState.persons[1].name} age={personsState.persons[1].age} click={nameChangeHandler.bind(this, 'Albertti')}>My hobbies: Trains</Person>
+        <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
+      </div> : null
+      }
     </div>
   );
 }
